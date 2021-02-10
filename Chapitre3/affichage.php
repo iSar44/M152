2cbe47462ccc7bb8b21d52b6151fd1b02b6b5732
iSar44 <saroukhanian.iliya@gmail.com>
2021-02-10 15:55:05 +0100

    file that displays the images dynamically

---
+++ Chapitre3/affichage.php
@@ -0,0 +1,26 @@
+<?php
+require_once("db_connection.php");
+
+$target_dir = "uploads/";
+
+while ($affichage = $imageChap3->fetch()) {
+
+    if(file_exists($target_dir . $affichage['nomMedia'])){
+        echo '<div class="col-sm-5">';
+        echo '<div class="panel panel-default">';
+        echo "<div class='panel-thumbnail'><img src='uploads/" . $affichage['nomMedia'] . "' class='img-responsive'></div>";
+        echo '<div class="panel-body">';
+        // echo '<p> class="lead">Urbanization</p>';
+        echo "<p>Date de création: " . $affichage['creationDate'] . " </p>";
+    
+        echo '<p><img src="assets/img/uFp_tsTJboUY7kue5XAsGAs28.png" height="28px" width="28px"></p>';
+        echo '</div>';
+        echo '</div>';
+    
+        echo '</div>';
+    }
+}
+
+
+
+?>
